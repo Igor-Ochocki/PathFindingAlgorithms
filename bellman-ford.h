e834c4969f2ceb5b4a75bfed6102daa968a4c6e5
Igor Ochocki <unigniu14@gmail.com>
2024-03-17 10:28:49 +0100

    Remove unneccessary files

--- bellman-ford.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BELLMAN_FORD_H_
-#define _BELLMAN_FORD_H_
-#include "maze_structure.h"
-#include "astar.h"
-#include "data_reader.h"
-
-node_t *bellmanFordSolve(adjacency_list_t *adjacencyList, linked_list_t *route, node_t *startNode, node_t *targetNode);
-
-#endif
